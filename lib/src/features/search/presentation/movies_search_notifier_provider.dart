4e05d52e5d14b5e1bf1043af38a276cb30fc2c78
Ruben Schlaen <gschlaen@agro.uba.ar>
2025-11-03 17:10:37 -0300

    Implement debouncing for search queries

--- lib/src/features/search/presentation/movies_search_notifier_provider.dart
+++ lib/src/features/search/presentation/movies_search_notifier_provider.dart
@@ -1,11 +1,31 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_browser_app/src/features/movies/data/repositories/movie_repository.dart';
 
 class MoviesSearchQueryNotifier extends Notifier<String> {
-  @override
-  String build() => '';
+  /// Used to debounce the input queries
+  Timer? _debounceTimer;
 
-  void update(String newValue) => state = newValue;
+  @override
+  String build() {
+    // Close the StreamController and cancel the subscriptions on dispose
+    ref.onDispose(() {
+      _debounceTimer?.cancel();
+    });
+    return '';
+  }
+
+  void update(String newValue) {
+    // Cancel the timer if it is active
+    if (_debounceTimer != null) {
+      _debounceTimer!.cancel();
+    }
+    _debounceTimer = Timer(const Duration(milliseconds: 500), () {
+      // only update the state once the query has been debounced
+      state = newValue;
+    });
+  }
 }
 
 /// Holds the raw search query as the user types
